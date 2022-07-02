00fd2ebfce5a80a3418b2dd6c36618145998df08
nirsade1 <nir.sade@gmail.com>
2022-07-02 20:38:33 +0300

    cardsjs

---
+++ js/arrey.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+let myArr = ["spade","clubs","hearts","diamonds"];
+
+for (let i=0; i<myArr.length;i++)
+{
+  for(let x=1;x<=13;x++)
+  {
+    //console.log(i.toString(),myArr[i]);
+    document.write(x.toString(),myArr[i] + "<br>");
+  }
+    
+ }
---
+++ js/whileloop.js
@@ -0,0 +1,18 @@
+
+
+
+
+let userName =prompt("Enter user name");
+let x = "nir";
+
+
+while(userName.trim().toLowerCase() !== x)
+{
+userName=prompt("enter agine:");
+
+
+}
+prompt("nir");
+
+
+
